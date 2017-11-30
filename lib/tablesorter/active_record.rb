620c03e4c947b76895a344864e32a613c473dcda
Stefan Bauckmeier <stefan.bauckmeier@akra.de>
2017-11-30 14:37:10 +0100

    refa: make use of active records sanitation

--- lib/tablesorter/active_record.rb
+++ lib/tablesorter/active_record.rb
@@ -3,7 +3,7 @@ module Tablesorter
     def table_sort(sort_attr, dir_attr, default_sort, default_dir)
       sort  = sort_attr.presence || default_sort
       dir   = dir_attr.presence  || default_dir
-      self.order("#{sort} #{dir}")
+      self.order(sort => dir)
     end
   end
 end
